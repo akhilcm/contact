885c04e02072c276cbd39ab16f250d593432f085
akhilcm <akhilcm777@gmail.com>
2019-08-27 11:52:21 +0530

    contact

--- app.js
+++ app.js
@@ -11,13 +11,33 @@ app.use(Express.static(__dirname+"/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+// For CORS,Pgm Line no 12 to 29
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200' );
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 Mongoose.connect("mongodb://localhost:27017/contactdb");
 
 const DataModel = Mongoose.model("userdata",{
-    ename:String,
-    eemail:String,
-    emsg:String,
-    emob:String
+    name:String,
+    email:String,
+    message:String,
+    mob:String
 });
 
 app.get('/',(req,res)=>{
